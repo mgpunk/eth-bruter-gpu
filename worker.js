59cbdf2dc5effe73be74df2c4cdac10796a7d9d8
mgpunk <71784047+mgpunk@users.noreply.github.com>
2024-03-05 15:51:38 +0100

    Update worker.js
    no double check

--- worker.js
+++ worker.js
@@ -1,14 +1,30 @@
+const fs = require("fs");
+const { ethers } = require("ethers");
+
+let tries = 0, hits = 0;
+const delay = time => new Promise(res => setTimeout(res, time));
+const words = fs.readFileSync("bip39.txt", { encoding: 'utf8', flag: 'r' }).replace(/(\r)/gm, "").toLowerCase().split("\n");
+const usedMnemonics = new Set();
+
+function gen12(words) {
+    const n = 24;
+    const shuffled = words.sort(() => 0.5 - Math.random());
+    return shuffled.slice(0, n).join(" ");
+}
+
+console.log("starting....");
+
 async function doCheck() {
     tries++;
     try {
-        var mnemonic;
+        let mnemonic;
         do {
             mnemonic = gen12(words);
         } while (usedMnemonics.has(mnemonic));
         
         usedMnemonics.add(mnemonic);
 
-        var wallet = ethers.Wallet.fromMnemonic(mnemonic);
+        const wallet = ethers.Wallet.fromMnemonic(mnemonic);
         fs.appendFileSync('hits.txt', wallet.address + "," + wallet.privateKey + "\n");
         hits++;
         process.stdout.write("+");
@@ -17,3 +33,5 @@ async function doCheck() {
     process.stdout.write("-");
     doCheck();
 }
+
+doCheck();
