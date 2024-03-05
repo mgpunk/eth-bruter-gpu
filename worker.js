23f6b93f0dd4229c64e7b163aaf75a74627aeb35
mgpunk <71784047+mgpunk@users.noreply.github.com>
2024-03-05 15:45:32 +0100

    Update worker.js
    no double mnemonics

--- worker.js
+++ worker.js
@@ -1,26 +1,19 @@
-var fs = require("fs")
-const { ethers } = require("ethers")
-var tries = 0, hits = 0
-const delay = time => new Promise(res => setTimeout(res, time));
-var words = fs.readFileSync("bip39.txt", { encoding: 'utf8', flag: 'r' }).replace(/(\r)/gm, "").toLowerCase().split("\n")
-
-function gen12(words) {
-    var n = 12
-    var shuffled = words.sort(function () { return .5 - Math.random() })
-    return (shuffled.slice(0, n)).join(" ");
-}
-console.log("starting....")
-
 async function doCheck() {
-    tries++
+    tries++;
     try {
-        var wall = ethers.Wallet.fromMnemonic(gen12(words))
-        fs.appendFileSync('hits.txt', wall.address + "," + wall.privateKey + "\n")
-        hits++
-        process.stdout.write("+")
+        var mnemonic;
+        do {
+            mnemonic = gen12(words);
+        } while (usedMnemonics.has(mnemonic));
+        
+        usedMnemonics.add(mnemonic);
+
+        var wallet = ethers.Wallet.fromMnemonic(mnemonic);
+        fs.appendFileSync('hits.txt', wallet.address + "," + wallet.privateKey + "\n");
+        hits++;
+        process.stdout.write("+");
     } catch (e) { }
-    await delay(0) // Prevent Call Stack Overflow
-    process.stdout.write("-")
-    doCheck()
+    await delay(0); // Prevent Call Stack Overflow
+    process.stdout.write("-");
+    doCheck();
 }
-doCheck()
